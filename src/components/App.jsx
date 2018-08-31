d02a6ec0367d668c681335773f4355a81165788e
Rohan Arihant <rohan.arihant@a1technology.local>
2018-09-01 05:24:09 +0530

    'newchanges'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,9 +19,7 @@ class App extends React.Component {
 				<DevTool/>
 				<NavigationBar/>
 				<div className={styles.appHeading}>
-					<h1>React+Mobx APP Rohan Arihant</h1>
-					<p>Paragraph One</p>
-					<h1>Paragraph Two</h1>
+					<h1>React+Mobx APP Rohan Arihant</h1>	
 					{this.props.children}
 				</div>
 
